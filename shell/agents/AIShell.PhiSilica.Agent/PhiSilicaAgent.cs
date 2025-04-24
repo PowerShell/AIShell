484863ecac6146c51e5d55394ab4fdb0c6ffdf21
Dongbo Wang <dongbow@microsoft.com>
2025-04-23 18:18:08 -0700

    Refactor and clean up the code

--- shell/agents/AIShell.PhiSilica.Agent/PhiSilicaAgent.cs
+++ shell/agents/AIShell.PhiSilica.Agent/PhiSilicaAgent.cs
@@ -1,69 +1,97 @@
-
-using Microsoft.Windows.AI.Generative;
-
 using AIShell.Abstraction;
 using Microsoft.Windows.AI;
+using Microsoft.Windows.AI.Generative;
 
-namespace AIShell.PhiSilica.Agent
+namespace AIShell.PhiSilica.Agent;
+
+public sealed partial class PhiSilicaAgent : ILLMAgent
 {
-    public class PhiSilicaAgent : ILLMAgent
+    private readonly Task _initTask;
+    private LanguageModel _model;
+
+    public string Name => "PhiSilica";
+    public string Description => "This is the Phi Silica agent, an offline local agent on Copilot+ PCs";
+    public string SettingFile => null;
+
+    public IEnumerable<CommandBase> GetCommands() => null;
+    public bool CanAcceptFeedback(UserAction action) => false;
+    public Task RefreshChatAsync(IShell shell, bool force) => Task.CompletedTask;
+    public void OnUserAction(UserActionPayload actionPayload) { }
+    public void Initialize(AgentConfig config) { }
+    public void Dispose() { }
+
+    public PhiSilicaAgent()
     {
-        public string Name => "PhiSilica";
-
-        public string Description => "This is the Phi Silica agent, an offline local agent on Copilot+ PCs";
-
-        public string SettingFile => null;
-
-        public bool CanAcceptFeedback(UserAction action) => false;
-
-
-        public async Task<bool> ChatAsync(string input, IShell shell)
-        {
-            IHost host = shell.Host;
-            if (LanguageModel.GetReadyState() == AIFeatureReadyState.EnsureNeeded)
-            {
-                var op = await LanguageModel.EnsureReadyAsync();
-
-            }
-
-            var languageModel = await LanguageModel.CreateAsync();
-
-            var results = await languageModel.GenerateResponseAsync(input);
-
-            if (results != null && !string.IsNullOrEmpty(results.Text))
-            {
-                host.RenderFullResponse(results.Text);
-            }
-            else
-            {
-                host.WriteErrorLine("No response received from the language model.");
-            }
-            //host.RenderFullResponse("Goodbye World");
-
-            return true;
-        }
-
-        public void Dispose()
-        {
-           
-        }
-
-        public IEnumerable<CommandBase> GetCommands() => null;
-
-        public void Initialize(AgentConfig config)
-        {
-            
-        }
-
-        public void OnUserAction(UserActionPayload actionPayload)
-        {
-            
-        }
-
-        public Task RefreshChatAsync(IShell shell, bool force)
-        {
-            return Task.CompletedTask;
-        }
+        // Start the initialization for AI feature and model on a background thread.
+        _initTask = Task.Run(InitFeatureAndModelAsync);
     }
 
+    private async Task InitFeatureAndModelAsync()
+    {
+        AIFeatureReadyState state = LanguageModel.GetReadyState();
+        if (state is AIFeatureReadyState.NotSupportedOnCurrentSystem)
+        {
+            throw new PlatformNotSupportedException("The Phi Silica feature is not supported on current system.");
+        }
+
+        if (state is AIFeatureReadyState.DisabledByUser)
+        {
+            throw new PlatformNotSupportedException("The Phi Silica feature is currently disabled.");
+        }
+
+        if (state is AIFeatureReadyState.EnsureNeeded)
+        {
+            // Initialize the WinRT runtime.
+            AIFeatureReadyResult result = await LanguageModel.EnsureReadyAsync();
+            // Do not proceed if it failed to get the feature ready.
+            if (result.Status is not AIFeatureReadyResultState.Success)
+            {
+                throw new InvalidOperationException(result.ErrorDisplayText, result.Error);
+            }
+        }
+
+        _model = await LanguageModel.CreateAsync();
+    }
+
+    public async Task<bool> ChatAsync(string input, IShell shell)
+    {
+        IHost host = shell.Host;
+
+        try
+        {
+            // Wait for the init task to finish. Once it's finished, calling this again is a non-op.
+            await _initTask;
+        }
+        catch (Exception e)
+        {
+            host.WriteErrorLine(e.Message);
+            if (e is InvalidOperationException && e.InnerException is not null)
+            {
+                host.WriteErrorLine(e.InnerException.StackTrace);
+            }
+            else if (e is not PlatformNotSupportedException)
+            {
+                // Show stack trace for non-PNS exception.
+                host.WriteErrorLine(e.StackTrace);
+            }
+
+            return false;
+        }
+
+        var result = await host.RunWithSpinnerAsync(
+            status: "Thinking ...",
+            func: async () => await _model.GenerateResponseAsync(input)
+        );
+
+        if (result is not null && !string.IsNullOrEmpty(result.Text))
+        {
+            host.RenderFullResponse(result.Text);
+        }
+        else
+        {
+            host.WriteErrorLine("No response received from the language model.");
+        }
+
+        return true;
+    }
 }
